aadda09a6025001d1267087a875db0a5929af615
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-02-05 11:56:57 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,17 @@
-'use strict'
+'use strict';
+function checkForSpam(message) {
+    let lowerCaseMessage = message.toLowerCase();
 
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding) * 2;
-    const borderWidth = parseFloat(border) * 2;
-    const totalWidth = contentWidth + paddingWidth + borderWidth;
-    return totalWidth;
+    if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+        return true;
+    } else {
+        return false;
+    }
 }
-
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book is on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true

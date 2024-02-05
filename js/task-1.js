5749cfa16ec5ade7634a7223fb37afd171981696
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-02-05 12:10:06 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     let totalPrice = quantity * pricePerDroid; 
     if (totalPrice > customerCredits) {
